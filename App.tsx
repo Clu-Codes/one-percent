c0af24ddf5baebb6f4a594210dad82ee32243d33
Clu-Codes <retsulcconnor@gmail.com>
2022-06-07 21:18:08 -0400

    initial commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
+// import AppLoading from 'expo-app-loading';
 import { StyleSheet, Text, View } from 'react-native';
+import tw from 'tailwind-react-native-classnames';
+import { useFonts, LuckiestGuy_400Regular } from '@expo-google-fonts/luckiest-guy';
 
 export default function App() {
+  let [fontsLoaded] = useFonts({
+    LuckiestGuy_400Regular,
+  });
+
+  if (!fontsLoaded) {
+    return <View><Text>Hi! I am Loading!</Text></View>;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <View style={tw.style('justify-center items-center bg-blue-700 flex-1')}>
+      <Text style={{fontFamily: 'LuckiestGuy_400Regular', fontSize: 90, color: 'white'}}>1%</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   text: {
+//     fontSize: 80
+//     font: : 
+//   },
+// });
