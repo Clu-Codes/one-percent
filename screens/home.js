b996365615553b5046f7624687c679b8fc82de00
Clu-Codes <retsulcconnor@gmail.com>
2022-06-12 20:14:38 -0400

    Initiated navigation process from clickable tasks on the home page.

--- screens/home.js
+++ screens/home.js
@@ -2,6 +2,7 @@ import { StatusBar } from "expo-status-bar";
 // import AppLoading from 'expo-app-loading';
 import {
   TouchableOpacity,
+  FlatList,
   StyleSheet,
   Text,
   View,
@@ -16,29 +17,66 @@ import {
 import Tasks from "../components/Tasks";
 
 export default function Home() {
-  let [fontsLoaded] = useFonts({
-    LuckiestGuy_400Regular,
-  });
-
-  if (!fontsLoaded) {
-    return (
-      <View>
-        <Text>Hi! I am having trouble!</Text>
-      </View>
-    );
-  }
+  const [task, setTask] = useState([
+    {
+      title: "Study Math",
+      streak: 4,
+      details: "Study math for the upcoming exam",
+      key: "1",
+    },
+    {
+      title: "Study Anestetics with Shay",
+      streak: 3,
+      details: "Review drug tables for the next exam",
+      key: "2",
+    },
+    {
+      title: "Practice Coding",
+      streak: 2,
+      details: "Try to continuously learn new technologies every week",
+      key: "3",
+    },
+    {
+      title: "Morning Yoga",
+      streak: 1,
+      details: "Yoga every morning with Shay before work",
+      key: "4",
+    },
+  ]);
 
   return (
     <ScrollView style={[styles.background, { flex: 1 }]}>
-      <View>
+      <FlatList>
+        data={task}
+        renderItem=
+        {({ item }) => (
+          <TouchableOpacity onPress={() => navigation.navigate("Tasks", item)}>
+            <Text>{item.title}</Text>
+          </TouchableOpacity>
+        )}
+      </FlatList>
+      {/* <View>
         <Text style={[styles.text, { marginVertical: 20, marginLeft: 15 }]}>
           Your Tasks:
         </Text>
       </View>
       <View style={tw.style("flex flex-col")}>
         <Tasks />
-      </View>
+      </View> */}
     </ScrollView>
+
+    // let [fontsLoaded] = useFonts({
+    //     LuckiestGuy_400Regular,
+    //   });
+
+    //   if (!fontsLoaded) {
+    //     return (
+    //       <View>
+    //         <Text>Hi! I am having trouble!</Text>
+    //       </View>
+    //     );
+    //   }
+
     // <View
     //   style={[
     //     tw.style("justify-center items-center flex-1"),
@@ -86,12 +124,14 @@ const styles = StyleSheet.create({
     backgroundColor: "#0c1e3e",
   },
   text: {
-    color: "#FFF1D0",
+    color: "#FFF",
     textShadowColor: "rgba(0, 0, 0, 0.25)",
     textShadowOffset: { width: 2, height: 2 },
     textShadowRadius: 2,
-    fontFamily: "LuckiestGuy_400Regular",
-    fontSize: 32,
+    // fontFamily: "LuckiestGuy_400Regular",
+    fontSize: 20,
+    paddingLeft: 10,
+    paddingVertical: 20,
   },
   textShadow: {
     textShadowColor: "rgba(0, 0, 0, 0.25)",
@@ -105,5 +145,16 @@ const styles = StyleSheet.create({
     backgroundColor: "#739E82",
     borderBottomWidth: 3,
     borderBottomColor: "#2C5530",
+  },
+  container: {
+    borderBottomWidth: 1.5,
+    borderTopWidth: 1.5,
+    borderColor: "rgba(99, 102, 241, 0.75);",
+  },
+  text: {
+    color: "#fff",
+    fontSize: 20,
+    paddingLeft: 10,
+    paddingVertical: 20,
   },
 });
